63e07c87d41bec8a76e9412753ca2765993adb3e
aryder1994 <aryder1994@gmail.com>
2017-01-24 12:03:51 -0600

    Add overflow flag

--- 32bit Adder.v
+++ 32bit Adder.v
@@ -1,10 +1,11 @@
 // 32-bit Adder
 
-module adder_32(a,b,cin,sum,cout);
+module adder_32(a,b,cin,sum,cout,overflow);
    input   [31:0] a, b;
    input          cin;
    output  [31:0] sum;
    output         cout;
+   output         overflow;
    wire	   [30:0] c;
 
    adder ADDER0(a[0], b[0], cin, sum[0], c[0]);
@@ -19,4 +20,5 @@ module adder_32(a,b,cin,sum,cout);
 
    adder ADDER31(a[31], b[31], c[30], sum[31], cout);
 
+   assign overflow = ((cout && !c[30]) || (!cout && c[30]));
 endmodule
