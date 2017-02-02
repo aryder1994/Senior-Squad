b9c340981f337eee432732197bc68997a505eea3
aryder1994 <aryder1994@gmail.com>
2017-02-02 13:32:37 -0600

    Add top level control signal comment

--- alu.v
+++ alu.v
@@ -1,3 +1,20 @@
+// Top level ALU
+//     sel0  sel1  sel2  sel3  sel4  sel5
+// AND    0     0     0     X     X     0
+// OR     1     0     0     X     X     0
+// XOR    X     1     0     X     X     0
+// SLL    X     1     1     X     X     0
+// SRA    0     0     1     X     X     0
+// SRL    1     0     1     X     X     0
+// SEQ    0     0     0     0     1     1
+// SNE    1     0     0     0     1     1
+// SLT    0     1     0	    0     1     1
+// SGT    1     1     0	    0     1     1
+// SLE    X     0     1	    0     1     1
+// SGE    X     1     1	    0     1     1
+// SUB    X     X     X 	  1     1     1
+// ADD    X     X     X	    X     0     1
+
 module alu(in1, in2, sel0, sel1, sel2, sel3, sel4, sel5, out);
     input    [31:0] in1;
     input    [31:0] in2;
