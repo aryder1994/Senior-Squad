1d6768a6c699e558ba0e8f15b014bb2c630a43a3
cellist1993 <cellist1993@yahoo.co.kr>
2017-02-09 11:25:48 -0600

    shifter for loop to parellel

--- multiplier_shifter.v
+++ multiplier_shifter.v
@@ -1,18 +1,20 @@
 module multiplier_shifter(in, out);
-     input   [64:0] in;
-     output  [64:0] out;
+     input   [67:0] in;
+     output  [67:0] out;
 
 
-     genvar i;
+     //genvar i;
 
-     generate
-     for (i = 0; i < 64; i=i+1)
-     begin
-        assign out[i] = in[i+1];
-     end
-     endgenerate
+     //generate
+     //for (i = 0; i < 64; i=i+1)
+     //begin
+     //   assign out[i] = in[i+1];
+     //end
+     //endgenerate
 
-     assign out[64] = in[64];
+     assign out[66:0] = in[67:1];
+
+     assign out[67] = in[67];
 endmodule
 
 
