d67f6c15cea961c5cea1a413ee1d8236bef012cc
cellist1993 <cellist1993@yahoo.co.kr>
2017-02-08 23:08:24 -0600

    64 bit to 65 bit

--- multiplier_shifter.v
+++ multiplier_shifter.v
@@ -1,18 +1,18 @@
 module multiplier_shifter(in, out);
-     input   [63:0] in;
-     output  [63:0] out;
+     input   [64:0] in;
+     output  [64:0] out;
 
 
      genvar i;
 
      generate
-     for (i = 0; i < 63; i=i+1)
+     for (i = 0; i < 64; i=i+1)
      begin
         out[i] = in[i+1];
      end
      endgenerate
 
-     out[63] = in[63];
+     out[64] = in[64];
 endmodule
 
 
