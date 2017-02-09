a473a46b2892a7aba28908430d638377be354aba
cellist1993 <cellist1993@yahoo.co.kr>
2017-02-08 23:22:08 -0600

    1 bit adder to 32 bit adder

--- multiplier_adder.v
+++ multiplier_adder.v
@@ -22,25 +22,15 @@ module multiplier_adder(a,b,sel0,sel1,out);
    wire    [31:0] b_new;
    wire    [31:0] c_in;
    wire    [31:0] sum;
+   wire    [31:0] overflow;
 
-   b_temp = ~b + 1;
-   c_in[0] = sel0;
+   assign b_temp = ~b;   
 
 
    mux_32 MUX_ADD_OR_SUBTRACT (sel0, b_temp, b, b_new);
 
-   genvar i;
-   adder ADDER0(a[0], b_new[0], sel0, sum[0], c[0]);
-                
-   generate
-   for (i = 1; i < 31; i=i+1)
-   begin
-           adder ADDERn(a[i], b_new[i], cin[i-1], sum[i], c[i]);
-   end
-   endgenerate
-
-   adder ADDER31(a[31], b_new[31], cin[30], sum[31], cout);
-
+   adder_32 ADDER0 (a, b_new, sel0, sum, cout, overflow);
+   
    mux_32 MUX_OUTPUT (sel1, a, sum, out);
 
 endmodule
