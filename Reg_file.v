a5a54b78bd869a2c9542ef6d4d8bd7fd5e1c5429
viclopez <victorlopez2016@u.northwestern.edu>
2017-02-22 18:35:48 -0600

    Add files via upload

--- Reg_file.v
+++ Reg_file.v
@@ -1,21 +1,38 @@
-module Reg_file(rA, rB, rW, busW, busA, busB, regWr, clk);
+module Reg_file(rA, rB, rW, busW, bus, busA, busB, regWr, clk);
   
   input [31:0] busW;
   input [4:0] rA, rB, rW; 
   input regWr, clk;
   
+  inout [31:0] bus;
+  
   output[31:0] busA, busB;
   reg [31:0] busA, busB;
   
-  always@(busW, rA, rB, rW, regWr, clk)
+  always@(busW, rA, rB, rW, regWr, bus, clk)
     begin
 
 			if(posedge clk & regWr == 1)
 			begin
 			
-			register REGISTERN(busW,busA, busB, regWr, clk);
+			register REGISTERn(busW, bus, regWr, clk);
 			
 			end
+			
+			if(posedge clk & rW == 1 & regWr == 1)
+			begin
+			
+			register REGISTERn(bus, busA, rW, clk);
+			
+			end
+			
+			elsif(posedge clk & rW == 0 & regWr == 1)
+			begin
+			
+			register REGISTERn(bus, busB, rW, clk);
+			
+			end
+			
       
     end
 endmodule
--- register.v
+++ register.v
@@ -5,20 +5,23 @@ module register(dataIn, dataOut, regWr, clk);
   
   output[31:0] dataOut
   reg dataOut;  
-  
-  
-  	genvar i;
-	generate
-		for (i = 0; i =< 31; i=i+1)  
-		begin
+  		
 		  always@(clk, dataIn, regWr)			
 			begin
+			
 				if(posedge clk & regWr == 1)
-				begin				
-					assign dataOut[i] = dataIn[i];
+				begin	
+				
+					dataOut = dataIn;
+				end
+				
+				else
+				begin
+				
+					dataOut;
+				
 				end
 			end
-		end	
-	endgenerate		      
+	      
 
 endmodule
